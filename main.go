b382d047481e793e9445c3a33f63d48e5504a1a4
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2024-11-04 18:32:00 +0300

    test

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main() {
 	http.Handle("/game000/css/", http.StripPrefix("/game000/css/", http.FileServer(http.Dir("css"))))
 	http.Handle("/game000/js/", http.StripPrefix("/game000/js/", http.FileServer(http.Dir("js"))))
 
-	http.Handle("/game000/", http.FileServer(http.Dir("./static")))
+	http.Handle("/game000/", http.StripPrefix("/game000/", http.FileServer(http.Dir("./static"))))
 	log.Fatal(http.ListenAndServe(":8091", nil))
 }
